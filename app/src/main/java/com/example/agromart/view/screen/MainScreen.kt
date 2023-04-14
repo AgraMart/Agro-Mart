953c367bf3de9a49e960420329faf38bfba08b82
Chandra-Mauli-Sharma <chandramaulisharma26@gmail.com>
2023-04-14 17:58:17 +0530

    First Commit

--- app/src/main/java/com/example/agromart/view/screen/MainScreen.kt
+++ app/src/main/java/com/example/agromart/view/screen/MainScreen.kt
@@ -1,6 +1,9 @@
 package com.example.agromart.view.screen
 
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Notifications
+import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -8,8 +11,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Green
 import androidx.compose.ui.res.painterResource
 import com.example.agromart.R
+import com.example.agromart.ui.theme.Golden
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -17,10 +23,20 @@ fun MainScreen() {
     Scaffold(topBar = {
         TopAppBar(title = { }, navigationIcon = {
             IconButton(onClick = { /*TODO*/ }) {
-                Icon(painter = painterResource(id = R.drawable.round_dashboard_24),tin)
+                Icon(
+                    painter = painterResource(id = R.drawable.round_dashboard_24),
+                    tint = com.example.agromart.ui.theme.Green,
+                    contentDescription = null
+                )
+            }
+        }, actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Rounded.Settings, tint = Color.LightGray, contentDescription = null)
+            }
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Rounded.Notifications, tint = Golden, contentDescription = null)
             }
         })
     }) {
-
     }
 }
