8afec32b15fb6ffdf4536b849bfca9ff62e0575f
Nil Mani <nilmani9430@gmail.com>
2023-04-16 00:08:32 +0530

    BlogPage Added

---
+++ app/src/main/java/com/example/agromart/view/screen/BlogPage.kt
@@ -0,0 +1,113 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.compose.rememberNavController
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BlogPage() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        // Top app bar
+        TopAppBar(
+            title = { Text(text = "Farming Blogs") },
+        )
+
+        // Main content
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 8.dp)
+        ) {
+            items(10) { index ->
+                BlogPost(
+                    title = "Blog Post $index",
+                    content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor.",
+                    imageUrl = "https://picsum.photos/200/300"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun BlogPost(title: String, content: String, imageUrl: String) {
+    var expanded by remember { mutableStateOf(false) }
+    val toggleExpansion: () -> Unit = { expanded = !expanded }
+
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
+
+        /*Image(
+            painter = rememberImagePainter(data = imageUrl),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+        )*/
+
+        Text(
+            text = if (expanded) content else content.take(100) + "...",
+            fontSize = 16.sp,
+            lineHeight = 24.sp,
+            modifier = Modifier.padding(top = 8.dp)
+        )
+
+        if (content.length > 100) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.End
+            ) {
+                Spacer(modifier = Modifier.weight(1f))
+                Button(
+                    onClick = toggleExpansion,
+                    modifier = Modifier.padding(top = 8.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor =  Color.Green),
+                    // Use ButtonDefaults to get default button colors and set the backgroundColor to green
+                ) {
+                    Text(text = if (expanded) "Read less" else "Read more")
+                }
+            }
+        }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun BlogPagePreview(){
+    BlogPage()
+}
