a541990b9e52818e30e217081d85ed998b7d7dfc
Jaskirat <sjaskirat461@gmail.com>
2023-04-16 18:05:14 +0530

    Sales Log Page Created

--- app/src/main/java/com/example/agromart/model/news/NewsResponse.kt
+++ app/src/main/java/com/example/agromart/model/news/NewsResponse.kt
@@ -7,23 +7,23 @@ data class NewsResponse (
 data class Datum (
     val title: String="",
     val author: String="",
-    val publishedDate: String,
+    val publishedDate: String="",
     val publishedDatePrecision: PublishedDatePrecision,
-    val link: String,
-    val cleanURL: String,
-    val excerpt: String,
-    val summary: String,
-    val rights: String,
+    val link: String="",
+    val cleanURL: String="",
+    val excerpt: String="",
+    val summary: String="",
+    val rights: String="",
     val rank: Long,
     val topic: Topic,
     val country: Country,
     val language: Language,
-    val authors: String,
-    val media: String,
-    val isOpinion: Boolean,
+    val authors: String="",
+    val media: String="",
+    val isOpinion: Boolean=false,
     val twitterAccount: String? = null,
-    val score: Double,
-    val id: String
+    val score: Double=0.0,
+    val id: String=""
 )
 
 enum class Country {
